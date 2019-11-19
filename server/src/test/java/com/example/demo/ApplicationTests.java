dd3576c6fa946641660883c4c771ab1a82d10342
Svetlana <svet.golovko@gmail.com>
2019-11-19 16:59:15 +0200

    changed 2.

--- server/src/test/java/com/example/demo/ApplicationTests.java
+++ server/src/test/java/com/example/demo/ApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@TestPropertySource("application.properties")
+@TestPropertySource("classpath:application-test.properties")
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTests {
--- server/src/test/java/com/example/demo/ClientControllerIntegrationTest.java
+++ server/src/test/java/com/example/demo/ClientControllerIntegrationTest.java
@@ -1,15 +1,25 @@
 package com.example.demo;
 
 import com.example.demo.controller.ClientController;
+import com.example.demo.model.Client;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = DemoApplication.class,
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+
 public class ClientControllerIntegrationTest {
 
     @Autowired
@@ -19,4 +29,20 @@ public class ClientControllerIntegrationTest {
     public void contexLoads() throws Exception {
         assertThat(controller).isNotNull();
     }
+
+    @Autowired private TestRestTemplate restTemplate;
+    @LocalServerPort
+    private int port;
+    @Test
+    public void whenGetRequestToUsers_thenCorrectResponse() throws Exception {
+        ResponseEntity<Iterable<Client>> responseEntity = restTemplate.exchange("http://localhost:" + port + "/clients", HttpMethod.GET,
+                null, new ParameterizedTypeReference<Iterable<Client>>() {
+        });
+        Iterable<Client> products = responseEntity.getBody();
+        Assertions
+                .assertThat(products)
+                .hasSize(4);
+
+      //  assertThat(products, hasItem(hasProperty("firstName", is("Tom"))));
+}
 }
