6135ab41cd3064f6702c289d630b5b2661a7a32d
Svetlana <svet.golovko@gmail.com>
2019-11-20 16:35:38 +0200

    changed 2.

--- server/src/test/java/com/example/demo/ApplicationTests.java
+++ server/src/test/java/com/example/demo/ApplicationTests.java
@@ -3,10 +3,9 @@ package com.example.demo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@TestPropertySource("classpath:application-test.properties")
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTests {
--- server/src/test/java/com/example/demo/ClientControllerIntegrationTest.java
+++ server/src/test/java/com/example/demo/ClientControllerIntegrationTest.java
@@ -11,15 +11,17 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertFalse;
 
+@TestPropertySource("classpath:application-test.properties")
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = DemoApplication.class,
-        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-
+@SpringBootTest(classes = DemoApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ClientControllerIntegrationTest {
 
     @Autowired
@@ -30,19 +32,54 @@ public class ClientControllerIntegrationTest {
         assertThat(controller).isNotNull();
     }
 
-    @Autowired private TestRestTemplate restTemplate;
+    @Autowired
+    private TestRestTemplate restTemplate;
     @LocalServerPort
     private int port;
+
     @Test
-    public void whenGetRequestToUsers_thenCorrectResponse() throws Exception {
+    public void whenGetRequestToClients_thenCorrectResponse() throws Exception {
         ResponseEntity<Iterable<Client>> responseEntity = restTemplate.exchange("http://localhost:" + port + "/clients", HttpMethod.GET,
                 null, new ParameterizedTypeReference<Iterable<Client>>() {
         });
-        Iterable<Client> products = responseEntity.getBody();
+        Iterable<Client> clients = responseEntity.getBody();
         Assertions
-                .assertThat(products)
-                .hasSize(4);
-
+                .assertThat(clients)
+                .hasSize(2);
       //  assertThat(products, hasItem(hasProperty("firstName", is("Tom"))));
-}
+    }
+
+    @Test
+    public void testAddClient()
+    {
+         Client client = new Client(Long.valueOf(1), "Lokesh", "Gupta");
+         Client savedClient = controller.addClient(client);
+         assertThat(savedClient.getFirstName().equals(client.getFirstName()));
+         assertThat(savedClient.getLastName().equals(client.getLastName()));
+    }
+
+    @Test
+    public void testAddClientsAndGetClient() {
+        // given
+        Client alex = new Client(Long.valueOf(2),"alex", "bond");
+        Client savedClient = controller.addClient(alex);
+        Client getClient = controller.getClient(savedClient.getId()).get();
+        assertThat(getClient.getLastName().equals(savedClient.getLastName()));
+    }
+
+    @Test
+    public void testAddClientAndDelete() {
+
+        Client alex = new Client(Long.valueOf(2),"alex", "bond");
+        Client savedClient = controller.addClient(alex);
+
+        Client getClient = controller.getClient(savedClient.getId()).get();
+        assertThat(getClient.getLastName().equals(savedClient.getLastName()));
+
+        ResponseEntity entity = controller.deleteClient(savedClient.getId());
+        assertThat(entity.getStatusCode() .equals(HttpStatus.OK ));
+
+        assertFalse(controller.getClient(savedClient.getId())==null);
+    }
+
 }
